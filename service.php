6fcaedac4bed84e7da338aa907b2c17741a2d70a
Danny Velasquez <sinoeproducciones@gmail.com>
2022-12-28 08:22:10 -0600

    first commit

---
+++ service.php
@@ -0,0 +1,43 @@
+<?php
+include 'inc/templates/header.php';
+?>
+
+<body>
+	<div class="container-xxl bg-white p-0">
+		<!-- Spinner Start -->
+		<div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+			<div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+				<span class="sr-only">Loading...</span>
+			</div>
+		</div>
+		<!-- Spinner End -->
+
+
+		<!-- Navbar & Hero Start -->
+		<div class="container-xxl position-relative p-0">
+			<?php
+			include 'inc/templates/navbar.php';
+			?>
+
+			<div class="container-xxl py-5 bg-dark hero-header mb-5">
+				<div class="container text-center my-5 pt-5 pb-4">
+					<h1 class="display-3 text-white mb-3 animated slideInDown">Servicios</h1>
+					<nav aria-label="breadcrumb">
+						<ol class="breadcrumb justify-content-center text-uppercase">
+							<li class="breadcrumb-item"><a href="#">Home</a></li>
+							<li class="breadcrumb-item text-white active" aria-current="page">Servicios</li>
+						</ol>
+					</nav>
+				</div>
+			</div>
+		</div>
+		<!-- Navbar & Hero End -->
+
+
+		<!-- Service Start -->
+		<?php
+		include 'inc/templates/servicios.php';
+		
+		
+	include 'inc/templates/footer.php';
+?>
